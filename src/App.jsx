8b6b14f889139ae7873da1cdf276422627011ebb
Sinan Noushad <130901572+SinanNoushad@users.noreply.github.com>
2025-06-10 14:51:24 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import { useState } from "react";
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <div className="h-[100%] w-[100%] bg-red-100">dfds</div>;
+}
+
+export default App;
